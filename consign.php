7b75d9801e3ab3669d10b681e3f7e242fdfe2908
Akriti Thakur <akritithakur17@gmail.com>
2018-04-15 15:25:51 +0530

    Add files via upload

---
+++ consign.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Basic Bootstrap Template</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!-- Optional Bootstrap theme -->
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+
+    <style>
+
+    .cons
+    {
+      text-align: center;
+
+    }
+
+    .cons1
+    {
+      text-align: center;
+       margin-left: 300px;
+      margin-right: 300px;
+    }
+    .crown___2qJ5m {
+    background-color: #fbf6f4;
+    border-bottom: 1px solid #e6e2e0;
+    font-size: 12px;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    padding: 7px 60px;
+}
+.hey
+{
+  margin-left: 300px;
+  margin-right: 300px;
+}
+.radio
+{
+  margin-left: 300px;
+  margin-right: 300px;
+}
+.text-center
+{
+  margin-left: 120px;
+  margin-right: 300px;
+}
+
+.stylish-input-group .input-group-addon{
+    background: white !important; 
+}
+.stylish-input-group .form-control{
+	border-right:0; 
+	box-shadow:0 0 0; 
+	border-color:#ccc;
+}
+.stylish-input-group button{
+    border:0;
+    background:transparent;
+}
+
+
+@media (min-width: 1240px)
+.crown-inner___RNy0Z {
+    margin: 0 auto;
+    max-width: 1240px;
+    padding: 0 20px;
+}
+.crown-inner___RNy0Z {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+}
+.crown-inner___RNy0Z {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+}
+.link___33BvM.secondary___3t2Ai {
+    color: #65605b;
+    text-decoration: none;
+}
+.sclass {
+  
+  
+  margin-right: 10px; /** Change this value to whatever you wish **/
+}
+
+
+  </style>
+
+
+</head>
+ <body>
+ <form action ="thanks.php" method="post" enctype="multipart/form-data">
+
+  <div class="crown___2qJ5m">
+  <div class="crown-inner___RNy0Z">
+  <a class="link___33BvM secondary___3t2Ai"  href="front.php"><span class="sclass">Home</span></a>
+  <a class="link___33BvM secondary___3t2Ai" href="directory.php"><span class="sclass">Directory</span></a>
+  <a class="link___33BvM secondary___3t2Ai" href="logout.php"><span class = "sclass">Logout</span></a>
+  <span class="sclass">|</span>
+  <a class="link___33BvM secondary___3t2Ai" rel="nofollow" href="help.php"><span class="sclass">Help</span></a>
+  </div>
+  </div>
+
+ <br>
+ <br>
+ <div class="cons">
+ <h3> <span> Why Consign with Us</span></h3> </div>
+ <br> 
+ <div class="cons1">
+ <h5> Our mission is to connect buyers with sellers of quality goods. If you own an item(s) you would like to consign, we can help make that key connection to our more than 4,000 auctioneers. <br><br>
+
+We assist in the sale of your property by providing a secure, neutral platform in which buyers and sellers worldwide may establish contact. Please complete the online form with item details. Your item will be listed and available for review and evaluation by all auctioneers on our site. If an auctioneer is interested in your item, their staff will get in touch with you. <br><br>
+
+Please Note: We do not act as an agent for your merchandise, nor do we in any way make representations with respect to the authenticity or value of your material goods. </h5>
+</div>
+<br>
+
+<div class="form-group___61dAR form-group">
+<label for="aquire" class="control-label">
+<div class="hey"><label for="description" class="control-label"><span>General Description</span></label><span class="help-block"><span class="help___v3L2Z">(<span>Please describe your item in as much details as possible. Please include details like what is it made of, who made it, when, the items condition, included documentations, marks or signatures</span>)</span></span>
+<textarea accept="*" autocomplete="on" autocapitalize="on" autocorrect="on" placeholder="" pattern="" type="textarea" id="description" class="form-control___2w-lK form-control" name="descrp">
+  
+</textarea></div>
+<br>
+<span class="hey">How did you acquire this item?</span>
+</label><div class="radio"><label title=""><input type="radio" styles="[object Object]" value="I aquired this item through a purchase" name="aquireGroup"><span>I bought it</span></label></div><div class="radio"><label title=""><input type="radio" value="I inherited this item" name="aquireGroup"><span>I inherited it</span></label></div><div class="radio"><label title=""><input type="radio" value="I found this item" name="aquireGroup"><span>I found it</span></label></div><div class="radio"><label title=""><input type="radio" styles="[object Object]" value="I was given this item" name="aquireGroup"><span>It was a gift</span></label></div></div>
+<div class="form-group___61dAR form-group"><label for="categories" class="control-label">
+
+<!--span>Category</span></label>
+<label title="">
+<input type="radio" name="cat" value="20th Century"><span>20th Century</span></label>
+<label title="">
+<input type="radio" name="cat" value="Autographs"><span>Autographs</span></label>
+<label  title=""><input type="radio" name="Books" value="Books"><span>Books</span></label><label class="checkbox-inline" title=""><input type="radio" name="Commercial" value="Commercial"><span>Commercial</span></label><label class="checkbox-inline" title=""><input type="radio" name="Decorative Arts" value="Decorative Arts"><span>Decorative Arts</span></label><label class="checkbox-inline" title=""><input type="radio" name="Entertainment" value="Entertainment"><span>Entertainment</span></label><label class="checkbox-inline" title=""><input type="radio" name="Fine Art" value="Fine Art"><span>Fine Art</span></label><label class="checkbox-inline" title=""><input type="radio" name="Furniture" value="Furniture"><span>Furniture</span></label><label class="checkbox-inline" title=""><input type="radio" name="Rugs/Carpets" value="Rugs/Carpets">Rugs/Carpets<span>Rugs/Carpets</span></label><label class="checkbox-inline" title=""><input type="radio" name="Toys" value="Toys"><span>Toys</span></label><label class="checkbox-inline" title=""><input type="radio" name="Other" value="Other"><span>Other</span></label></div></div-->
+
+<span class="hey">Photo</span>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+<div class="hey">
+<input type="file" name="fileToUpload" id="fileToUpload"> </div>
+<
+<br>
+
+
+<div class="text-center">
+        <button class="btn btn-default">Submit</button>
+</div>
+  <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    
+
+</body>
+</html>
---
+++ directory.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Auction House Directory</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!-- Optional Bootstrap theme -->
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.15/datatables.min.css"/>
+    <style>
+.container
+{
+  margin-left: 200px;
+  margin-right: 150px;
+  margin-top: 150px;
+}
+    .cons
+    {
+      text-align: center;
+
+    }
+
+    .cons1
+    {
+      text-align: center;
+       margin-left: 300px;
+      margin-right: 300px;
+    }
+    .crown___2qJ5m {
+    background-color: #fbf6f4;
+    border-bottom: 1px solid #e6e2e0;
+    font-size: 12px;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    padding: 7px 60px;
+
+
+}
+
+
+.hey
+{
+  margin-left: 300px;
+  margin-right: 300px;
+}
+.radio
+{
+  margin-left: 300px;
+  margin-right: 300px;
+}
+.text-center
+{
+  margin-left: 120px;
+  margin-right: 300px;
+}
+
+.stylish-input-group .input-group-addon{
+    background: white !important; 
+}
+.stylish-input-group .form-control{
+  border-right:0; 
+  box-shadow:0 0 0; 
+  border-color:#ccc;
+}
+.stylish-input-group button{
+    border:0;
+    background:transparent;
+}
+
+
+@media (min-width: 1240px)
+.crown-inner___RNy0Z {
+    margin: 0 auto;
+    max-width: 1240px;
+    padding: 0 20px;
+}
+.crown-inner___RNy0Z {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+}
+.crown-inner___RNy0Z {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+}
+.link___33BvM.secondary___3t2Ai {
+    color: #65605b;
+    text-decoration: none;
+}
+.sclass {
+  
+  
+  margin-right: 10px; /** Change this value to whatever you wish **/
+}
+.live-item-bidding___2dKxe .footer___YQHNW .estimate___2AED- {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+    text-align: center;
+    text-transform: uppercase;
+    font-family: "HelveticaNeueW01-65Medi";
+}
+
+.item-live-right___277t0 {
+    overflow: hidden;
+    -webkit-box-flex: 0;
+    -ms-flex: 0 0 40%;
+    flex: 0 0 40%;
+    max-width: 500px;
+    margin-top: -410px;
+    margin-left: 750px;
+}
+.live-item-right-top___25W-J {
+    border-bottom: 1px solid #e6e2e0;
+}
+.live-item-header___3qTMy {
+    display: block;
+    padding: 0;
+}
+.live-item-header___3qTMy h1 {
+    font-size: 28px;
+    font-family: "Futura W01 Book";
+    font-weight: normal;
+    letter-spacing: 1px;
+    margin: 0 0 20px;
+    line-height: 33px;
+    margin-left: 0px;
+    margin-top: 0px;
+    margin-bottom: 40px;
+}
+.live-item-right-top___25W-J {
+    border-bottom: 1px solid #e6e2e0;
+}
+.item-live-right___277t0 {
+    overflow: hidden;
+    -webkit-box-flex: 0;
+    -ms-flex: 0 0 40%;
+    flex: 0 0 40%;
+    max-width: 500px;
+}
+.live-item-detail-specs___3XDvf span {
+    margin-right: 4px;
+    float: left;
+}
+.live-item-detail-specs___3XDvf {
+    font-size: 14px;
+    color: #65605b;
+    margin: 0 0 20px;
+    width: 100%;
+    height: 20px;
+}
+.live-item-bidding___2dKxe {
+    background: #fbf6f4;
+    border: 1px solid #e6e2e0;
+    border-radius: 0.43em;
+    min-height: 288px;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    margin-bottom: 30px;
+}
+* {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+
+.live-item-bidding___2dKxe .header___3glH8 {
+    -webkit-box-flex: 0;
+    -ms-flex: 0 0 auto;
+    flex: 0 0 auto;
+    border-bottom: 1px solid rgba(170, 170, 170, 0.2);
+    padding: 15px 30px;
+    font-size: 12px;
+    font-family: "HelveticaNeueW01-55Roma";
+    color: #65605b;
+}
+
+.live-item-bidding___2dKxe .header___3glH8 .countdown___XYSA0 {
+    display: block;
+}
+
+.p
+{
+  display: block;
+  margin: 0 8px 0 0;
+
+}
+.count-down___qpsYU {
+    display: block;
+}
+.live-item-bidding___2dKxe .body___us5bz {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+    padding: 30px;
+}
+.live-item-bidding___2dKxe .footer___YQHNW {
+    -webkit-box-flex: 0;
+    -ms-flex: 0 0 auto;
+    flex: 0 0 auto;
+    border-top: 1px solid rgba(170, 170, 170, 0.2);
+    padding: 20px 30px;
+    font-size: 11px;
+    letter-spacing: 1.12px;
+    font-family: "HelveticaNeueW01-55Roma";
+    color: #65605b;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-flow: wrap;
+    flex-flow: wrap;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+}
+* {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+.live-item-right-top___25W-J .register-for-auction___3AvQn {
+    cursor: pointer;
+    text-decoration: underline;
+    display: block;
+    color: #0097ba;
+    padding-bottom: 25px;
+}
+.register-for-auction___ds_Fj {
+    font-size: 13px;
+    font-family: "HelveticaNeueW01-55Roma";
+}
+.link___33BvM {
+    color: #0097ba;
+    cursor: pointer;
+    text-decoration: underline;
+}
+a {
+    background-color: transparent;
+}
+.live-item-detail-specs___3XDvf .date____QUF6 {
+    display: inline-block;
+
+
+}
+.live-item-header___3qTMy {
+    display: block;
+    padding: 0;
+}
+.live-item-bidding___2dKxe .footer___YQHNW .estimate___2AED- {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+    text-align: center;
+    text-transform: uppercase;
+    font-family: "HelveticaNeueW01-65Medi";
+}
+
+.live-item-bidding___2dKxe .header___3glH8 {
+    font-size: 12px;
+    font-family: "HelveticaNeueW01-55Roma";
+    color: #65605b;
+  }
+  .count-down___qpsYU .second___3F3Oe {
+    margin: 0 8px 0 0;
+    text-transform: uppercase;
+}
+.live-item-detail-specs___3XDvf span {
+    margin-right: 4px;
+    float: left;
+}
+
+.count-down___qpsYU .starts___1K22P {
+    font-size: 10px;
+    text-transform: uppercase;
+}
+  </style>
+</head>
+<body>
+<div class="crown___2qJ5m">
+  <div class="crown-inner___RNy0Z">
+  <a class="link___33BvM secondary___3t2Ai" href="front.php"><span class="sclass">Home</span></a>
+  
+ 
+  <a class="link___33BvM secondary___3t2Ai" rel="noopener"  href="consign.php"><span class="sclass">Consign</span></a>
+  <a class="link___33BvM secondary___3t2Ai" href="logout.php"><span class="sclass">Logout</span></a><span class="sclass">|</span>
+  <a class="link___33BvM secondary___3t2Ai" rel="nofollow" href="help.php"><span class="sclass">Help</span></a>
+  </div>
+  </div>
+
+
+ <div class="col-md-12">
+        <h3>AUCTION HOUSE DIRECTORY</h3>
+        <br>
+<table class='table' id="table">
+    <thead>
+    <th>Auction House Name</th>
+<th>Description</th>
+<th>Address </th>
+<th>Contact No.</th>
+     
+
+        
+    </thead>
+ 
+
+<?php
+
+ $con = mysql_connect("localhost","root","");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+ 
+ $db_selected = mysql_select_db('SEN', $con); 
+ 
+if (!$db_selected) {  
+die ('database not selected : ' . mysql_error()); }
+ 
+ 
+
+$query = "select * from auctionH";
+
+$result=mysql_query($query);
+
+$i = 1; 
+ echo "<tbody>";
+while($row = mysql_fetch_array($result))
+  {
+	
+  echo "<tr>";
+  echo "<td>" . $row['name'] . "</td>";
+  echo "<td>" . $row['des'] . "</td>";
+  echo "<td>" . $row['address'] . "</td>";
+  echo "<td>" . $row['contact'] . "</td>";
+  
+ 
+  
+	  
+ 
+ 
+  echo "</tr>";
+  $i++;
+  }
+  
+ 
+
+             
+
+echo "</tbody>";
+echo "</table>";
+
+  
+  echo "<input type='hidden' name='action' value='' id='action' />"; 
+echo "</form>";
+
+mysql_close($con);
+
+
+
+?>
+
+</table>
+</div>
+<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.15/datatables.min.js"></script>
+<script>
+    $(document).ready(function(){
+      $('#table').DataTable();
+    }); 
+</script>
+
+</body>
+
+</html>
